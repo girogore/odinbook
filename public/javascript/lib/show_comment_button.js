3b034e350cf4b1231a9e9562eac8705627363d1b
Stephen Johnson <stephenjohnson505@gmail.com>
2025-01-28 22:53:10 -0800

    Show Comment text now updates amount dynamically

--- public/javascript/lib/show_comment_button.js
+++ public/javascript/lib/show_comment_button.js
@@ -2,24 +2,23 @@
   let buttons = document.querySelectorAll('[id^="show_comment_"]');
   buttons.forEach(element => {
     element.addEventListener("click", () => {
-      const regex = /Show Comments/;
-      const match = element.innerHTML.match(regex);
-      if (element.dataset.val == undefined) element.dataset.val = element.innerHTML;
-      if (match) {
-        const regex2 = /id="show_comment_([^"]+)"/;
-        const match2 = element.outerHTML.match(regex2);
-        post_name = match2[1];
+      const regex_show = /Show Comments/;
+      const match_show = element.innerHTML.match(regex_show);
+      if (match_show) {
+        const regex_post = /id="show_comment_([^"]+)"/;
+        post_name = element.outerHTML.match(regex_post)[1];
         element.innerHTML = "Hide Comments";
         element.href = "posts/"+post_name+"/comments";
-        //post_comments_path(post_id: post.id)
       }
       else {
-        element.innerHTML = element.dataset.val;
         turbo_frame = element.dataset.turboFrame;    
-        element.href = "javascript:void(0)";    
-        let frame = document.querySelector('#'+turbo_frame)
+        let frame = document.querySelector("#"+turbo_frame)
+        number_comments = frame.childElementCount-1;
+        
+        // Set button text, stop the redirect, clear frame
+        element.innerHTML = "Show Comments ("+number_comments+")";
+        element.href = "javascript:void(0)";
         frame.innerHTML = "";
-        // post_comments_path(post_id: post.id)
       }
     })
   })
